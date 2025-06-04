8558517c7cca8dfcfd2e199ddfb4e9270979edaf
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-06-04 18:25:41 -0300

    Add floating room menu on the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import FloatingRoomMenu from "@/components/floating-room-menu";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:34px_44px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
         <div className="relative z-10">{children}</div>
+        <FloatingRoomMenu />
       </body>
     </html>
   );

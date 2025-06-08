5bd23d24b8b2be4c03188a8aa47b995f84609742
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-06-08 01:51:17 -0300

    Make z-index smaller

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           <AudioProvider>
             <div
               className={cn(
-                "absolute bottom-0 left-0 right-0 top-0 bg-[size:34px_44px]",
+                "-z-20 absolute bottom-0 left-0 right-0 top-0 bg-[size:34px_44px]",
                 "bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]",
                 "dark:bg-[linear-gradient(to_right,#ffffff08_1px,transparent_1px),linear-gradient(to_bottom,#ffffff08_1px,transparent_1px)]"
               )}

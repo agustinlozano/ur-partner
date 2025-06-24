9be4bb38b4492c1fe3e8314cdb3d0d08a5f84ec5
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-06-24 20:14:59 -0300

    Add a new env var to check limit

--- lib/env.ts
+++ lib/env.ts
@@ -3,4 +3,7 @@ export const enviroment = process.env.NODE_ENV;
 // Lambda upload configuration
 export const LAMBDA_UPLOAD_ENDPOINT =
   process.env.NEXT_PUBLIC_LAMBDA_UPLOAD_ENDPOINT;
-export const USE_LAMBDA_UPLOAD = true;
+export const USE_LAMBDA_UPLOAD = false; // Now we use API route as proxy
+
+// Rate limiting configuration
+export const RATE_LIMIT_ENDPOINT = process.env.LAMBDA_RATE_LIMIT_ENDPOINT;

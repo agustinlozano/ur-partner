baea16f5325b4285b62b4e20fd12ddda9bfac8fa
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-07-07 22:00:23 -0300

    Disable toggle theme button on `realtime` paths

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -5,16 +5,25 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
-export function ThemeToggle() {
+export function ThemeToggle({ forceDark = false }: { forceDark?: boolean }) {
   const { setTheme, theme } = useTheme();
+
+  React.useEffect(() => {
+    if (forceDark) setTheme("dark");
+  }, [forceDark, setTheme]);
 
   return (
     <Button
       variant="outline"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => {
+        if (!forceDark) setTheme(theme === "light" ? "dark" : "light");
+        else toast.warning("This is a dark-only mode page :)");
+      }}
       className="relative"
+      // disabled={forceDark}
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />

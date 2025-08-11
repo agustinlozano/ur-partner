cb39bcb5c856350f4c23bbaab622f4ea6fc2e6fa
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-08-10 23:07:35 -0300

    Add Unsplash icon component

---
+++ components/icons.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+export const UnsplashIcon = ({ className }: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={cn("size-4", className)}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 11h5v4h6v-4h5v9h-16zm5 -7h6v4h-6z" />
+  </svg>
+);
